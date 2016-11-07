24db7861870451d6a338de74378c1449e423caaa
zhaoxuesong <18513263563@163.com>
2016-11-07 13:54:56 +0800

    Initial commit

---
+++ src/main/java/com/weareone/Easy2payApplication.java
@@ -0,0 +1,12 @@
+package com.weareone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Easy2payApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Easy2payApplication.class, args);
+	}
+}
